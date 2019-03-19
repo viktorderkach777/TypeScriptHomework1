1876c671e384d31280671b5b966874705f687e68
viktorderkach7777 <viktorderkach7777@gmail.com>
2019-03-19 16:45:48 +0200

    Create 3 functions

--- TypeScriptHomework1/app.js
+++ TypeScriptHomework1/app.js
@@ -1,31 +1,23 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
+var Greeter = /** @class */ (function () {
+    function Greeter(element) {
         this.element = element;
         this.element.innerHTML += "The time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
     }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
+    Greeter.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
+    };
+    Greeter.prototype.stop = function () {
         clearTimeout(this.timerToken);
-    }
-
-}
-
-
+    };
+    return Greeter;
+}());
 function greeter(person) {
     return "Hello, " + person;
 }
-
 function greet(elementId) {
     var inputValue = document.getElementById(elementId)["value"];
     if (inputValue.trim() == "") {
@@ -33,51 +25,36 @@ function greet(elementId) {
     }
     document.getElementById("third").innerText = greeter(inputValue);
 }
-
 //let fr = "11";
 //if ((<HTMLInputElement>document.getElementById('first')).value!= null) {
 //    fr = (<HTMLInputElement>document.getElementById('first')).value;
 //};
-
-let fr, sc, th;
-
-function firstFunction(): void {  
-   
+var fr, sc, th;
+function firstFunction() {
     fr = document.getElementById("first")["value"];
     console.log(fr + " " + typeof fr);
-};
-
-function secondFunction(): void {
+}
+;
+function secondFunction() {
     sc = document.getElementById("second")["value"];
     console.log(sc);
-};
-
-function thirdFunction(): void {
+}
+;
+function thirdFunction() {
     th = document.getElementById("third")["value"];
     console.log(th);
-};
-
-function myFunction(): void {
-
+}
+;
+function myFunction() {
     console.log("myFunction");
-};
-
-
-
-
-window.onload = () => {
+}
+;
+window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
-
-   
-   
-   
-         
     //document.getElementById("first").addEventListener("click", myFunction);
-   
     //firstFunction
-
     //function convertFromCelsius() {
     //    if (cels.value) {
     //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
@@ -88,9 +65,7 @@ window.onload = () => {
     //        kelv.value = "";
     //    }
     //}
-
     //function convertFromFarenheit() {
-
     //    if (farenh.value) {
     //        cels.value = ((+farenh.value - 32) / 1.8).toFixed(3);
     //        kelv.value = (+cels.value + 273.15).toFixed(3);
@@ -100,9 +75,7 @@ window.onload = () => {
     //        kelv.value = "";
     //    }
     //}
-
     //function convertFromKelvin() {
-
     //    if (kelv.value) {
     //        cels.value = (+kelv.value - 273.15).toFixed(3);
     //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
@@ -112,14 +85,11 @@ window.onload = () => {
     //        farenh.value = "";
     //    }
     //}       
-
-
-
-    function mathOp(x: number, y: number, operation: (a: number, b: number) => number): number {
-
-        let result = operation(x, y);
+    function mathOp(x, y, operation) {
+        var result = operation(x, y);
         return result;
     }
-    console.log(mathOp(10, 20, (x, y) => x + y)); // 30 
-    console.log(mathOp(10, 20, (x, y) => x * y)); // 200 
+    console.log(mathOp(10, 20, function (x, y) { return x + y; })); // 30 
+    console.log(mathOp(10, 20, function (x, y) { return x * y; })); // 200 
 };
+//# sourceMappingURL=app.js.map
