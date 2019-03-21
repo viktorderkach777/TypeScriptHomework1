54328c4ea6ccb00dad970507bfe77c3de5181ae8
viktorderkach7777 <viktorderkach7777@gmail.com>
2019-03-21 14:58:22 +0200

    Add third function

--- TypeScriptHomework1/app.js
+++ TypeScriptHomework1/app.js
@@ -1,72 +1,12 @@
 /// <reference path="scripts/typings/jquery/jquery.d.ts" />
-var Greeter = /** @class */ (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
-function greeter(person) {
-    return "Hello, " + person;
-}
-function greet(elementId) {
-    var inputValue = document.getElementById(elementId)["value"];
-    if (inputValue.trim() == "") {
-        inputValue = "World";
-    }
-    document.getElementById("third").innerText = greeter(inputValue);
-}
-//let fr = "11";
-//if ((<HTMLInputElement>document.getElementById('first')).value!= null) {
-//    fr = (<HTMLInputElement>document.getElementById('first')).value;
-//};
-//let fr, sc, th;
-//function GetFunction(idName:string): void {
-//    let fr = (<HTMLInputElement>document.getElementById(idName)).value;
-//    console.log(fr + " " + typeof fr);   
-//};
-function firstFunction() {
-    var fr = document.getElementById('first').value;
-    var sc = document.getElementById("second")["value"];
-    var th = document.getElementById("third")["value"];
-    var resultString = (fr + sc + th).toLocaleUpperCase();
-    document.getElementById('result').value = resultString;
-    console.log(resultString + " " + typeof resultString);
-}
-;
-function secondFunction(getFn, algoFn, setFn) {
-    //let fr = (<HTMLInputElement>document.getElementById('first')).value;
-    //let sc = document.getElementById("second")["value"];
-    //let th = document.getElementById("third")["value"];    
-    //let list: string[] = [fr, sc, th];
+function universalFunction(getFn, algoFn, setFn) {
     setFn(algoFn(getFn()));
-    console.log($('#first').val());
-    //let list = GetList();
-    //let resString = "";
-    //let maxNum = Math.max(list[0].length, list[1].length, list[2].length);   
-    //for (var i = 0; i < maxNum; i++) {
-    //    for (var j = 0; j < list.length; j++) {
-    //        if (list[j].length > i) {
-    //            resString += list[j][i];               
-    //        }
-    //    }        
-    //} 
-    //let resString = Func2(list);
-    //SetList(resString);
-    //(<HTMLInputElement>document.getElementById('result')).value = resString;
-    //console.log(resString);
 }
 ;
+function Func1(list) {
+    var resultString = (list[0] + list[1] + list[2]).toLocaleUpperCase();
+    return resultString;
+}
 function Func2(list) {
     var resString = "";
     var maxNum = Math.max(list[0].length, list[1].length, list[2].length);
@@ -79,11 +19,24 @@ function Func2(list) {
     }
     return resString;
 }
-function SetList(resString) {
-    document.getElementById('result').value = resString;
-    console.log(resString);
+function Func3(list) {
+    var resString = UniqueCharacters(list[0]) + UniqueCharacters(list[1]) + UniqueCharacters(list[2]);
+    resString = resString.split('').sort().join('');
+    return resString;
 }
-function GetList() {
+function UniqueCharacters(test) {
+    var temp = "";
+    for (var i = 0; i < test.length; i++) {
+        if (temp.indexOf(test.charAt(i)) == -1) {
+            temp = temp + test.charAt(i);
+        }
+    }
+    return temp;
+}
+function SetValues(resString) {
+    document.getElementById('result').value = resString;
+}
+function GetValues() {
     var fr = document.getElementById('first').value;
     var sc = document.getElementById("second")["value"];
     var th = document.getElementById("third")["value"];
@@ -91,75 +44,4 @@ function GetList() {
     return list;
 }
 ;
-function thirdFunction() {
-    var fr = document.getElementById('first').value;
-    var sc = document.getElementById("second")["value"];
-    var th = document.getElementById("third")["value"];
-    var list = [fr, sc, th];
-    //function onlyUnique(value, index, self) {
-    //    return self.indexOf(value) === index;
-    //}
-    function uniques(arr) {
-        var a = [];
-        for (var i = 0, l = arr.length; i <= l; i++)
-            if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
-                a.push(arr[i]);
-        return a;
-    }
-    //var uniq = list.filter(onlyUnique);
-    //let resString = "";
-    //for (var i = 0; i < list.length; i++) {
-    //    console.log(split(uniques[i]));
-    //}
-    //(<HTMLInputElement>document.getElementById('result')).value = resString;
-    //console.log(resString);
-}
-;
-function myFunction() {
-    console.log("myFunction");
-}
-;
-window.onload = function () {
-    //var el = document.getElementById('content');
-    //var greeter = new Greeter(el);
-    //greeter.start();
-    //document.getElementById("first").addEventListener("click", myFunction);
-    //firstFunction
-    //function convertFromCelsius() {
-    //    if (cels.value) {
-    //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
-    //        kelv.value = (+cels.value + 273.15).toFixed(3);
-    //    }
-    //    else {
-    //        farenh.value = "";
-    //        kelv.value = "";
-    //    }
-    //}
-    //function convertFromFarenheit() {
-    //    if (farenh.value) {
-    //        cels.value = ((+farenh.value - 32) / 1.8).toFixed(3);
-    //        kelv.value = (+cels.value + 273.15).toFixed(3);
-    //    }
-    //    else {
-    //        cels.value = "";
-    //        kelv.value = "";
-    //    }
-    //}
-    //function convertFromKelvin() {
-    //    if (kelv.value) {
-    //        cels.value = (+kelv.value - 273.15).toFixed(3);
-    //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
-    //    }
-    //    else {
-    //        cels.value = "";
-    //        farenh.value = "";
-    //    }
-    //}       
-    function mathOp(x, y, operation) {
-        var result = operation(x, y);
-        return result;
-    }
-    console.log(mathOp(10, 20, function (x, y) { return x + y; })); // 30 
-    console.log(mathOp(10, 20, function (x, y) { return x * y; })); // 200 
-};
 //# sourceMappingURL=app.js.map
--- TypeScriptHomework1/app.ts
+++ TypeScriptHomework1/app.ts
@@ -1,105 +1,18 @@
 ﻿/// <reference path="scripts/typings/jquery/jquery.d.ts" />
 
 
-
-
-
-
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-
-function greeter(person) {
-    return "Hello, " + person;
-}
-
-function greet(elementId) {
-    var inputValue = document.getElementById(elementId)["value"];
-    if (inputValue.trim() == "") {
-        inputValue = "World";
-    }
-    document.getElementById("third").innerText = greeter(inputValue);
-}
-
-//let fr = "11";
-//if ((<HTMLInputElement>document.getElementById('first')).value!= null) {
-//    fr = (<HTMLInputElement>document.getElementById('first')).value;
-//};
-
-//let fr, sc, th;
-
-//function GetFunction(idName:string): void {
-
-//    let fr = (<HTMLInputElement>document.getElementById(idName)).value;
-//    console.log(fr + " " + typeof fr);   
-//};
-
-
-
-
-function firstFunction(): void {  
-   
-    let fr = (<HTMLInputElement>document.getElementById('first')).value;
-    let sc = document.getElementById("second")["value"];
-    let th = document.getElementById("third")["value"];
-   
-
-    let resultString = (fr + sc + th).toLocaleUpperCase();
-
-    (<HTMLInputElement>document.getElementById('result')).value = resultString;
-    console.log(resultString + " " + typeof resultString);
+function universalFunction(getFn: () => string[], algoFn: (a: string[]) => string, setFn: (b: string) => void): void {
+    
+    setFn(algoFn(getFn()));    
 };
 
-function secondFunction(getFn: () => string[], algoFn: (a: string[]) => string, setFn: (b: string) => void  ): void {
-    //let fr = (<HTMLInputElement>document.getElementById('first')).value;
-    //let sc = document.getElementById("second")["value"];
-    //let th = document.getElementById("third")["value"];    
 
-    //let list: string[] = [fr, sc, th];
-    setFn(algoFn(getFn()));
-    console.log($('#first').val());
+function Func1(list: string[]): string {
 
-    //let list = GetList();
-    //let resString = "";
-    //let maxNum = Math.max(list[0].length, list[1].length, list[2].length);   
+    let resultString = (list[0] + list[1] + list[2]).toLocaleUpperCase();
+    return resultString;
+}
 
-    //for (var i = 0; i < maxNum; i++) {
-
-    //    for (var j = 0; j < list.length; j++) {
-
-    //        if (list[j].length > i) {
-    //            resString += list[j][i];               
-    //        }
-    //    }        
-    //} 
-
-    //let resString = Func2(list);
-
-    //SetList(resString);
-
-    //(<HTMLInputElement>document.getElementById('result')).value = resString;
-    //console.log(resString);
-};
 
 function Func2(list: string[]): string {
 
@@ -115,124 +28,45 @@ function Func2(list: string[]): string {
                 resString += list[j][i];
             }
         }
-    }    
+    }
 
     return resString;
 }
 
-function SetList(resString:string): void {
-    (<HTMLInputElement>document.getElementById('result')).value = resString;
-    console.log(resString);
+
+function Func3(list: string[]): string {
+
+    let resString = UniqueCharacters(list[0]) + UniqueCharacters(list[1]) + UniqueCharacters(list[2]);
+    resString = resString.split('').sort().join('');   
+
+    return resString;
 }
 
 
-function GetList(): string[] {
+function UniqueCharacters(test: string): string {
+
+    let temp = "";
+
+    for (let i = 0; i < test.length; i++) {
+        if (temp.indexOf(test.charAt(i)) == - 1) {
+            temp = temp + test.charAt(i);
+        }
+    }
+
+    return temp;
+}
+
+
+function SetValues(resString: string): void {
+    (<HTMLInputElement>document.getElementById('result')).value = resString;     
+}
+
+
+function GetValues(): string[] {
     let fr = (<HTMLInputElement>document.getElementById('first')).value;
     let sc = document.getElementById("second")["value"];
     let th = document.getElementById("third")["value"];
 
     let list: string[] = [fr, sc, th];
     return list;
-};
-
-function thirdFunction(): void {
-    let fr = (<HTMLInputElement>document.getElementById('first')).value;
-    let sc = document.getElementById("second")["value"];
-    let th = document.getElementById("third")["value"];
-
-    let list: string[] = [fr, sc, th];
-   
-
-    //function onlyUnique(value, index, self) {
-    //    return self.indexOf(value) === index;
-    //}
-
-    function uniques(arr) {
-        var a = [];
-        for (var i = 0, l = arr.length; i <= l; i++)
-            if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
-                a.push(arr[i]);
-        return a;
-    }
-   
-   
-    //var uniq = list.filter(onlyUnique);
-
-    //let resString = "";
-    
-
-    //for (var i = 0; i < list.length; i++) {
-    //    console.log(split(uniques[i]));
-    //}
-
-
-    //(<HTMLInputElement>document.getElementById('result')).value = resString;
-    //console.log(resString);
-};
-
-function myFunction(): void {
-
-    console.log("myFunction");
-};
-
-
-
-
-window.onload = () => {
-    //var el = document.getElementById('content');
-    //var greeter = new Greeter(el);
-    //greeter.start();
-
-   
-   
-   
-         
-    //document.getElementById("first").addEventListener("click", myFunction);
-   
-    //firstFunction
-
-    //function convertFromCelsius() {
-    //    if (cels.value) {
-    //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
-    //        kelv.value = (+cels.value + 273.15).toFixed(3);
-    //    }
-    //    else {
-    //        farenh.value = "";
-    //        kelv.value = "";
-    //    }
-    //}
-
-    //function convertFromFarenheit() {
-
-    //    if (farenh.value) {
-    //        cels.value = ((+farenh.value - 32) / 1.8).toFixed(3);
-    //        kelv.value = (+cels.value + 273.15).toFixed(3);
-    //    }
-    //    else {
-    //        cels.value = "";
-    //        kelv.value = "";
-    //    }
-    //}
-
-    //function convertFromKelvin() {
-
-    //    if (kelv.value) {
-    //        cels.value = (+kelv.value - 273.15).toFixed(3);
-    //        farenh.value = (+cels.value * 1.8 + 32).toFixed(3);
-    //    }
-    //    else {
-    //        cels.value = "";
-    //        farenh.value = "";
-    //    }
-    //}       
-
-
-
-    function mathOp(x: number, y: number, operation: (a: number, b: number) => number): number {
-
-        let result = operation(x, y);
-        return result;
-    }
-    console.log(mathOp(10, 20, (x, y) => x + y)); // 30 
-    console.log(mathOp(10, 20, (x, y) => x * y)); // 200 
 };
